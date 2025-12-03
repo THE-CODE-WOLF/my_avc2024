af48a673f7234bdc8f57227891f6db89a3e96681
CyberWolf <64925733+THE-CODE-WOLF@users.noreply.github.com>
2025-12-03 13:56:00 -0500

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -1,6 +1,6 @@
 
 #Replace with your file path below
-fileName = "C:/Users/woods/PycharmProjects/my_avc2024/Puzzles/1.1.txt"
+fileName = "PycharmProjects/my_avc2024/Puzzles/1.1.txt"
 
 with open(fileName, encoding="utf-8") as file:
     lines = [line.rstrip() for line in file]
